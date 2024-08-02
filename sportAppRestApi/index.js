3f9132bb003de98a40a1951dda2f46b484c1e690
StefanDimitrov04 <s.dimitrov2004@gmail.com>
2024-08-02 13:33:33 +0300

    vercel commit

--- sportAppRestApi/index.js
+++ sportAppRestApi/index.js
@@ -6,6 +6,9 @@ const routes = require('./routes');
 
 const app = express();
 app.use(cors());
+app.use(cors({
+    origin: 'https://sports-zone-d6zj.vercel.app'
+  }));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
